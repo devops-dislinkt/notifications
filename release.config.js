811aebe4082451ce923f84dad5803333fd736a48
specnazm <52412989+specnazm@users.noreply.github.com>
2022-06-23 19:45:09 +0200

    ci: added pipeline (#3)
    * ci: added pipeline
    
    * fix: changed branch name to master

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	branches: ["master", {"name": "dev", "prerelease": true}],
+	repositoryUrl: "https://github.com/devops-dislinkt/user-post.git",
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		["@semantic-release/github", {
+			assets: [
+				{"path": "dist/*.gz", "label": "build"},
+						]
+		}
+		]
+	]
+}
